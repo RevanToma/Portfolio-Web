d0f88a14891a05f0b622fb59a4bc947d02c683c8
RevanToma <tamrevan@gmail.com>
2022-11-06 21:00:31 +0100

    Fixed the show resume button bugg were it didnt toggle on/off, added some styles

--- resume.js
+++ resume.js
@@ -1,35 +1,37 @@
 
-const showLess = document.querySelector('.showLess');
-const aboutMeButton = document.querySelector('.moreAboutMe');
-let resumeInfo = document.querySelector('.resumeInfo');
-const skillSection = document.querySelector('.skillSection');
-const personalInfo = document.querySelector('.personalInfo');
+const showLess = document.querySelector('.showLess'); // Show  less button for resume section.
+const aboutMeButton = document.querySelector('.moreAboutMe'); // Show resume button.
+const resumeInfo = document.querySelector('.resumeInfo'); // Resume info article.
+const skillSection = document.querySelector('.skillSection'); // Skill section.
+const personalInfo = document.querySelector('.personalInfo'); // Default personal info display when resume info is not showing.
 
 
 
 showLess.style.display = 'none';
 
-// load my resume json on DOM on click.
+// Load my resume json on DOM on click.
+// Hide other unnecessary elements
+// Someway to load json resume only once? toggle show/hide only after that ? faster than reloading it?
 aboutMeButton.addEventListener('click', () => {
-
     getResume();
-    resumeShowLess();
     skillSection.style.visibility = 'hidden';
     personalInfo.style.visibility = 'hidden';
-    // aboutMeButton.setAttribute('disabled', '');
     aboutMeButton.style.display = 'none';
     showLess.style.display = '';
-
-
-
+    resumeShowLess(); // Hide resume after loading it once.
 
 });
 
 // hide the resume json information.
 function resumeShowLess() {
-    showLess.addEventListener('click', () => {
 
-        location.reload();
+    showLess.addEventListener('click', () => {
+        resumeInfo.innerHTML = '';
+        skillSection.style.visibility = '';
+        personalInfo.style.visibility = '';
+        showLess.style.display = 'none';
+        aboutMeButton.style.display = '';
+
     });
 }
 
@@ -51,19 +53,17 @@ async function getResume() {
 
         for (let myInfo of data.myInformation) {
 
-            let myPersonalInformation = "<ul><h3>" + myInfo.name + "</h3><li><a href=" + myInfo.url + " target=_blank>Portfolio Page</a></li><li>Email: " +
-                myInfo.email + "</li><li>Phone Num: " + myInfo.phone + "</li><li>Label: " + myInfo.label + "</li><li>City: "
-                + myInfo.location.region + "</li><p>" + myInfo.location.address + "</p><p>Postal-Code: " + myInfo.location.postalCode + "</p><p>Region : "
-                + myInfo.location.city + "</p></ul>";
+            let myPersonalInformation = `<ul><h3>  ${myInfo.name} </h3><li><a href=" ${myInfo.url} " target=_blank>Portfolio Page</a></li><li>Email: ${myInfo.email}</li>
+            <li>Phone Num: ${myInfo.phone} </li><li>Label: ${myInfo.label} </li><li>City: ${myInfo.location.region} </li><p> ${myInfo.location.address} </p>
+            <p>Postal-Code: ${myInfo.location.postalCode} </p><p>Region: ${myInfo.location.city}</p></ul>`;
 
             resumeInfo.innerHTML += myPersonalInformation;
 
         }
         for (let job of data.jobs) {
 
-            let Job = "<ul><h3>Job: " + job.name + "</h3><p>Position: " + job.position + "<p><p>Start date: "
-                + job.startDate + "</p><p>End date: " + job.endDate + "</p><p>Position: "
-                + job.position + "</p></ul>";
+            let Job = `<ul><h3>Job:${job.name}</h3><p>Position: ${job.position}<p><p>Start date: ${job.startDate}</p><p>End date: ${job.endDate}</p><p>Position: ${job.position}
+            </p></ul>`;
             resumeInfo.innerHTML += Job;
 
 
@@ -73,16 +73,16 @@ async function getResume() {
         for (let datas of data.educations) {
 
 
-            let Educations = "<ul><h3>" + datas.institution + "</h3><li><a href=" + datas.url + " target=_blank>URL</a></li><li>Education: " +
-                datas.education + "</li><p>Start date: " + datas.startDate + "</p><p>End date: " + datas.endDate + "</p><p>Position: "
-                + datas.position + "</p></ul>";
+            let Educations = `<ul><h3> ${datas.institution} </h3><li><a href=" ${datas.url} " target=_blank>URL</a></li><li>Education: ${datas.education}</li>
+            <p>Start date: ${datas.startDate}</p><p>End date: ${datas.endDate}</p><p>Position: ${datas.position}</p></ul>`;
+
             resumeInfo.innerHTML += Educations;
         }
         for (let skill of data.skills) {
 
 
-            let Skill = "<ul><h3>Skills: " + skill.name + "</h3><p>Level: " + skill.level + "</p><li>Keywords : " +
-                skill.keywords + "</li><p><li>Competence: " + skill.competence + "</li></ul>";
+            let Skill = `<ul><h3>Skills: ${skill.name}</h3><p>Level: ${skill.level}</p><li><em>Keywords:</em> ${skill.keywords}</li><p><li><em>Competence:</em>
+             ${skill.competence}</li></ul>`;
 
             resumeInfo.innerHTML += Skill;
 
@@ -90,15 +90,15 @@ async function getResume() {
         for (let language of data.languages) {
 
 
-            let Language = "<ul><h3>Languages:</h3><p>Name : " + language.language + "</p><p>Fluency : " + language.fluency + "</p></ul>";
+            let Language = `<ul><h3>Languages:</h3><p>Name:  ${language.language}</p><p>Fluency : ${language.fluency}</p></ul>`;
 
             resumeInfo.innerHTML += Language;
 
         }
         for (let interest of data.interests) {
 
-            let Interest = `<ul><h3>Interests:</h3><li> ${interest.keywords[0]} </li><li> ${interest.keywords[1]} </li><li> ${interest.keywords[2]} </li>
-            <li> ${interest.keywords[3]} </li><li> ${interest.keywords[4]} </li></ul>`;
+            let Interest = `<ul><h3>Interests:</h3><li> ${interest.keywords[0]} </li><li> ${interest.keywords[1]} </li><li> ${interest.keywords[2]}</li>
+            <li> ${interest.keywords[3]} </li><li> ${interest.keywords[4]} </li></ul> `;
 
 
             resumeInfo.innerHTML += Interest;
